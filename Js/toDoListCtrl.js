cdceaeb71f54e08a7428cb33937a5782ba7ef28f
djhvscf <networsociales@hotmail.com>
2014-07-09 10:01:07 -0600

    Update

--- Js/toDoListCtrl.js
+++ Js/toDoListCtrl.js
@@ -1,155 +1,68 @@
-var myTodoList = angular.module('TodoListApp',[]).controller('toDoListCtrl', function($scope, $timeout) {
-    $scope.idTodo = 1;
-    $scope.searchTodo = "";
-    $scope.todos = [{id: $scope.idTodo++, name: "Aprender angular", categorie: "Proyecto 3", done: true},
-                    {id: $scope.idTodo++, name: "Cocinar el almuerzo", categorie: "Cocina", done: false},
-                    {id: $scope.idTodo++, name: 'Tarea de mate', categorie: "Colegio", done: false}];
+angular.module('mainModule', ['LocalStorageModule','ngAnimate'])
+    .controller('toDoListCtrl', ['$scope', '$window', '$timeout', 'localStorageService', function($scope, $window, $timeout, localStorageSvc) {
 
-	$scope.categories = [{
-        name: 'Tarea',
-        value: '0',
-        style: 'color: #ffffff;' + 'background-color: #1e90ff;',
-        checked: false
-    }, {
-        name: 'Cocina',
-        value: '1',
-        style: 'color: #3c763d;' + 'background-color: #dff0d8;',
-        checked: false
-    },{
-        name: 'Colegio',
-        value: '2',
-        style: 'color: #31708f;' + 'background-color: #d9edf7;',
-        checked: false
-    },{
-        name: 'Casa',
-        value: '3',
-        style: 'color: #8a6d3b;' + 'background-color: #fcf8e3;',
-        checked: false
-    },{
-        name: 'Proyecto 3',
-        value: '4',
-        style: 'color: #a94442;' + 'background-color: #f2dede;',
-        checked: false
-    }];
-	
-	
-    $scope.isActive = true;
-	$scope.isActiveDone = true;
-	
-	$scope.select = function(value){
-        //for es más rápido que angular.forEach()
-        for (i = 0; i < $scope.todos.length; i++) {
-            if($scope.todos[i].value == value){
-                $scope.todos[i].visible = (!$scope.todos[i].visible) ? true : false;
+    $scope.tareas = [];
+
+    $scope.tipos = [{name: 'Programación', value: '0', style: 'color: #ffffff;' + 'background-color: #1e90ff;', checked: false}, 
+					{name: 'Biología', value: '1', style: 'color: #3c763d;' + 'background-color: #dff0d8;', checked: false},
+					{name: 'Filosofía', value: '2', style: 'color: #31708f;' + 'background-color: #d9edf7;', checked: false},
+					{name: 'Historia', value: '3', style: 'color: #8a6d3b;' + 'background-color: #fcf8e3;', checked: false},
+					{name: 'Matemática', value: '4', style: 'color: #a94442;' + 'background-color: #f2dede;',checked: false}];
+
+    $scope.init = function() {
+        if(localStorageSvc.get('tareas')) {
+            $scope.tareas = localStorageSvc.get('tareas');
+            for (i = 0; i < $scope.tareas.length; i++) {
+                $scope.tareas[i].visible = false;
             }
         }
     }
 	
-	$scope.check = function(item){
+    $scope.check = function(item){
         item.checked = item.checked ? false : true;
     }
-	
-    $scope.addTodo = function() {
-        if ($scope.todoName === "") {
-            return false;
-        }else if($scope.todoName === undefined && $scope.todoCategorie === undefined) {
-            return false;
-        }else if($scope.todoCategorie === undefined)
-        {
-            return false;
-        }
 
-        $scope.todos.push({
-            id: $scope.idTodo++,
-            name: $scope.todoName,
-            categorie: $scope.todoCategorie,
-            done: false
-        });
-		/*debugger;
-		var t = JSON.stringify($scope.todos);
-		localStorage.setItem("todos", t);
-		var personaGuardada = localStorage.getItem("todos");
-		console.log(typeof personaGuardada); //string
- 
-		var personaGuardada = JSON.parse(personaGuardada);
-		/*localStorageSvc.set('todos', $scope.todos);*/
+    $scope.add = function(){
+        if($scope.nombre){
+            var tarea = {};
+            tarea.id = $scope.tareas.length;
+            tarea.name = $scope.nombre;
+            tarea.checked = false;
+            tarea.style = $scope.selected.style;
+            tarea.value = $scope.selected.value;
 
-        $scope.todoName = '';
-        $scope.todoCategorie = 0;
-    }
-
-    $scope.remove = function(todo)
-    {
-        $scope.isActive = !$scope.isActive;
-
-        var deleteTodo = function() {
-            var index = $scope.todos.indexOf(todo);
-
-            if (index > -1) {
-                $scope.todos.splice(index, 1);
+            for (i = 0; i < $scope.tipos.length; i++) {
+                if($scope.tipos[i].value == tarea.value){
+                    tarea.visible = $scope.tipos[i].checked;
+                }
             }
-			$scope.isActive = true;
+
+            $scope.tareas.push(tarea);
+            localStorageSvc.set('tareas', $scope.tareas);
         }
-
-        $timeout(deleteTodo, 3000);
     }
-	
-	/*$scope.init = function() {
-        if(localStorageSvc.get('todos')) {
 
-            $scope.todos = localStorageSvc.get('todos');
+    $scope.edit = function() {
+        localStorageSvc.set('tareas', $scope.tareas);
+    }
 
-            //resetea la visibilidad de las tareas;
-            for (i = 0; i < $scope.todos.length; i++) {
-                $scope.todos[i].visible = false;
+    $scope.remove = function(item){
+        for (i = 0; i < $scope.tareas.length; i++) {
+            if($scope.tareas[i].id == item.id){
+                $scope.tareas.splice(i,1);
+            }
+        }
+        localStorageSvc.set('tareas', $scope.tareas);
+    }
+
+    $scope.select = function(value){
+        for (i = 0; i < $scope.tareas.length; i++) {
+            if($scope.tareas[i].value == value){
+                $scope.tareas[i].visible = (!$scope.tareas[i].visible) ? true : false;
             }
         }
     }
+
+	$scope.init();
 	
-	$scope.init();*/
-});
-
-myTodoList.controller('toDoListEditCtrl', function($scope, $timeout) {
-    $scope.editorEnabled = false;
-
-    $scope.enableEditor = function(todo)
-    {
-        $scope.editorEnabled = true;
-
-        $scope.todoNameEdit = todo.name;
-        $scope.todoCategorieEdit = todo.categorie;
-    };
-
-    $scope.disableEditor = function()
-    {
-        $scope.editorEnabled = false;
-    };
-
-    $scope.save = function()
-    {
-        if ($scope.todoNameEdit=== "") {
-            return false;
-        }
-
-        $scope.todo.name = $scope.todoNameEdit;
-        $scope.todo.categorie = $scope.todoCategorieEdit;
-
-        $scope.disableEditor();
-    };
-
-    $scope.removeDone = function(todo)
-    {
-        $scope.isActiveDone = !$scope.isActiveDone;
-
-        var deleteTodo = function() {
-            var index = $scope.todos.indexOf(todo);
-
-            if (index > -1) {
-                $scope.todos.splice(index, 1);
-            }
-			$scope.isActiveDone = true;
-        }
-
-        $timeout(deleteTodo, 3000);
-    }
-});
+}]);
